23edcb999da9820a1c5c2e939f534d2fd6f4aef5
barnachum203 <barnachum203@gmail.com>
2021-07-28 14:17:18 +0300

    Setup listings-detail-page

--- buy-and-sell/src/app/listings-detail-page/listings-detail-page.component.ts
+++ buy-and-sell/src/app/listings-detail-page/listings-detail-page.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from "@angular/router";
+import { fakeListings } from '../fake-data';
+import { Listing } from '../types';
 
 @Component({
   selector: 'app-listings-detail-page',
@@ -7,9 +10,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListingsDetailPageComponent implements OnInit {
 
-  constructor() { }
+  listing: any;
+
+  constructor(
+    private route: ActivatedRoute,
+    ) { }
 
   ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.listing = fakeListings.find(listing => listing.id === id)
+  
   }
 
 }
