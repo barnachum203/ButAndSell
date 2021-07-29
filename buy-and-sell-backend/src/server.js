fdfd616678616de51a5a347975abd7a0f1f4bd2b
barnachum203 <barnachum203@gmail.com>
2021-07-29 14:11:47 +0300

    Setup getting listings

--- buy-and-sell-backend/src/server.js
+++ buy-and-sell-backend/src/server.js
@@ -1,22 +1,23 @@
-import Hapi from "@hapi/hapi";
-import routes from "./routes";
+import Hapi from '@hapi/hapi';
+import routes from './routes';
 
 const start = async () => {
-  const server = Hapi.Server({
-    port: 8000,
-    host: "localhost",
-  });
+    const server = Hapi.server({
+        port: 8000,
+        host: 'localhost',
+    });
 
-  routes.forEach((route) => server.route(route));
+    routes.forEach(route => server.route(route));
 
-  await server.start();
-  console.log(`Server is listening on port ${server.info.uri}`);
-};
+    await server.start();
+    console.log(`Server is listening on ${server.info.uri}`);
+}
 
-process.on("unhandledRejection", (err) => {
-  console.log(err);
-  process.exit(1);
+process.on('unhandledRejection', err => {
+    console.log(err);
+    process.exit(1);
 });
 
-
 start();
+
+
