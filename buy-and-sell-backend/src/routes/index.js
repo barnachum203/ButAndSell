3e08e1b3e85fbc9a9c5c50c44e91bb80bff1b3e8
barnachum203 <barnachum203@gmail.com>
2021-07-30 16:54:27 +0300

    getting users listings by id

--- buy-and-sell-backend/src/routes/index.js
+++ buy-and-sell-backend/src/routes/index.js
@@ -1,9 +1,11 @@
 import { getAllListingsRoutes } from "./getAllListings";
 import { getListingRoute } from "./getListing"
 import { addViewToListingRoute } from "./addViewToListing"
+import { getUserListingsRoute } from "./getUserListings"
 
 export default[
     getAllListingsRoutes,
     getListingRoute,
     addViewToListingRoute,
+    getUserListingsRoute,
 ];
