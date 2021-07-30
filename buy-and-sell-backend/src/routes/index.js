65db8f6e11784fa4b495832acf36002cb126ec0b
barnachum203 <barnachum203@gmail.com>
2021-07-30 16:42:35 +0300

    Setup incremeting views

--- buy-and-sell-backend/src/routes/index.js
+++ buy-and-sell-backend/src/routes/index.js
@@ -1,6 +1,9 @@
 import { getAllListingsRoutes } from "./getAllListings";
 import { getListingRoute } from "./getListing"
+import { addViewToListingRoute } from "./addViewToListing"
+
 export default[
     getAllListingsRoutes,
     getListingRoute,
+    addViewToListingRoute.
 ];
