743030989a37a196ccb91823cbe329680e1721db
barnachum203 <barnachum203@gmail.com>
2021-07-30 17:11:06 +0300

    Create new listing for user using UUID

--- buy-and-sell-backend/src/routes/index.js
+++ buy-and-sell-backend/src/routes/index.js
@@ -2,10 +2,13 @@ import { getAllListingsRoutes } from "./getAllListings";
 import { getListingRoute } from "./getListing"
 import { addViewToListingRoute } from "./addViewToListing"
 import { getUserListingsRoute } from "./getUserListings"
+import { createNewListingRoute } from "./createNewListing"
+
 
 export default[
     getAllListingsRoutes,
     getListingRoute,
     addViewToListingRoute,
     getUserListingsRoute,
+    createNewListingRoute,
 ];
