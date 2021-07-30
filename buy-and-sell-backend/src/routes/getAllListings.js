75f829ed762f33d3fd61ed46f104660ef52bf5b5
barnachum203 <barnachum203@gmail.com>
2021-07-30 16:26:25 +0300

    Connecting to mysql Setup getting all listings and listings by id

--- buy-and-sell-backend/src/routes/getAllListings.js
+++ buy-and-sell-backend/src/routes/getAllListings.js
@@ -1,9 +1,12 @@
-import { fakeListings, fakeMyListings } from "./fake-data";
+import {db} from "../database";
 
 export const getAllListingsRoutes = {
   method: "GET",
   path: "/api/listings",
-  handler: (req, h) => {
-    return fakeListings;
+  handler: async(req, h) => {
+    const {results} = await db.query(
+      'SELECT * FROM listings'
+    );
+    return results;
   },
 };
