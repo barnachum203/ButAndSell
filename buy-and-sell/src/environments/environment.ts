f6aba56ae3cc8d9de581ac1a3e2af2b5ed4d7281
barnachum203 <barnachum203@gmail.com>
2021-07-31 16:18:01 +0300

    Setup Firebase Auth users now need to sing in to see the content

--- buy-and-sell/src/environments/environment.ts
+++ buy-and-sell/src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyD2KoYmf27rNISsB_QW4RHSa07-gn_BHIU',
+    authDomain: 'buyandsell-a826f.firebaseapp.com',
+    projectId: 'buyandsell-a826f',
+    storageBucket: 'buyandsell-a826f.appspot.com',
+    messagingSenderId: '656779875157',
+    appId: '1:656779875157:web:d30d9b76a436b544d655dc',
+    measurementId: 'G-PQG40WMKJQ',
+  },
 };
 
 /*
